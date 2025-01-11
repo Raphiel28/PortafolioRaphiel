4ebd7b0e350e2b795020cac6407d944bc38702f0
Raph <raphielburdier72@gmail.com>
2025-01-10 20:02:41 -0400

    Error enlace prueba 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -93,7 +93,7 @@ function Presentation(){
   </p>
   <div style={{height:'15px'}}></div>
 <div className='d-flex flex-row align-items-center'>
-<a href={`${window.location.origin}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
+<a href={`${process.env.PUBLIC_URL}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
 <svg className={classIco}  style={{width:'1.5rem', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
           fill="#8750F7" stroke="#8750F7" stroke-width="2" />
@@ -159,7 +159,7 @@ function Presentation(){
   <div style={{height:'15px'}}></div>
 <div className='d-flex flex-row align-items-center'>
 
-<a href={`${window.location.origin}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
+<a href={`${process.env.PUBLIC_URL}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
 <svg className={classIco}  style={{width:'1.5rem', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
           fill="#8750F7" stroke="#8750F7" stroke-width="2" />
@@ -226,7 +226,7 @@ Creo soluciones intuitivas que simplifican la experiencia de usuario  mediante a
 <div style={{height:'15px'}}></div>
 <div className='d-flex flex-row align-items-center'>
 
-<a href={`${window.location.origin}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
+<a href={`${process.env.PUBLIC_URL}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
 <svg className={classIco}  style={{width:'1.5rem', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
           fill="#8750F7" stroke="#8750F7" stroke-width="2" />
@@ -294,7 +294,7 @@ Creo soluciones intuitivas que simplifican la experiencia de usuario  mediante a
 <div style={{height:'15px'}}></div>
 <div className='d-flex flex-row align-items-center'>
 
-<a href={`${window.location.origin}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
+<a href={`${process.env.PUBLIC_URL}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-4' style={buttonStyle}> Descargar CV
 <svg className={classIco}  style={{width:'1.5rem', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
           fill="#8750F7" stroke="#8750F7" stroke-width="2" />
@@ -362,7 +362,7 @@ Creo soluciones intuitivas que simplifican la experiencia de usuario  mediante a
   <div style={{height:'15px'}}></div>
 <div className='d-flex flex-row align-items-center'>
 
-<a href={`${window.location.origin}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-3' style={buttonStyle}> Descargar CV
+<a href={`${process.env.PUBLIC_URL}/archivos/CV-DesarrolladorDeSoftwareRaphiel.pdf`} download="CV-DesarrolladorDeSoftwareRaphiel.pdf" onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} onMouseDown={MouseDown} onMouseUp={MouseUp} className='button-DefaultHome me-3' style={buttonStyle}> Descargar CV
 <svg className={classIco}  style={{width:'1.5rem', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
           fill="#8750F7" stroke="#8750F7" stroke-width="2" />
